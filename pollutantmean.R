ca20849af333a404f8acdd6ab6752cbba04e95a8
jerry dumblauskas <jdumblauskas@gmail.com>
2015-02-14 18:21:20 -0600

    clean file

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,13 +1,11 @@
 library(stringr)
 
-pollutantmean <- function(directory, pollutant, id = 332) {
+pollutantmean <- function(directory, pollutant, id = 1:332) {
     ## 'directory' is a character vector of length 1 indicating
     ## the location of the CSV files
-    
-    zzz <- paste("~/r_work/" ,toString(directory), sep = "")
-    print (zzz)
-    setwd(zzz)
-    print (getwd())
+
+    setwd(paste("~/r_work/" ,toString(directory), sep = ""))
+
     
     ## 'pollutant' is a character vector of length 1 indicating
     ## the name of the pollutant for which we will calculate the
@@ -19,17 +17,22 @@ pollutantmean <- function(directory, pollutant, id = 332) {
     ## Return the mean of the pollutant across all monitors list
     ## in the 'id' vector (ignoring NA values)
     
+    # dummy var to do merging
     tst <- c(NA)
+    
+    # loop all files -- build a giant data fram
     for (idx in id) {
         idxy <-str_pad(idx, 3, pad = "0")
-        print (idxy)
-        qq<-read.csv(paste(idxy,".csv", sep = ""))
+
+        ds<-read.csv(paste(idxy,".csv", sep = ""))
         if (is.na(tst)) {
             tst <- c(1)
-            my_merged_data <-qq
+            my_merged_data <-ds
             next
         }
-        my_merged_data <-rbind(my_merged_data, qq)
+        
+        # perform a mean on the final data set
+        my_merged_data <-rbind(my_merged_data, ds)
     }
     
     # get the mean
