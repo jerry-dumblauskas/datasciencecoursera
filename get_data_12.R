c5af41ae97f6fe00b3e6bbe29b460acff2f9b2d0
jerry dumblauskas <jdumblauskas@gmail.com>
2015-03-09 06:23:26 -0500

    clean up, and prep for next week

--- get_data_12.R
+++ get_data_12.R
@@ -10,7 +10,7 @@ ggg<-subset(fff,fff$VAL == 24 & fff$TYPE==1)
 
 library(xlsx)
 raw_d<-read.xlsx('l_data.xlsx', sheetIndex=1, header=TRUE)
-dat<-raw_d[ 7:15, 18:23]
+dat<-raw_d[ 18:23, 7:15]
 print (sum(dat$Zip*dat$Ext,na.rm=T) )
 
 library(XML)
@@ -21,3 +21,20 @@ rootNode<-xmlRoot(l_res)
 xmlName(rootNode)
 
 zippy<-xpathSApply(rootNode, "//zipcode", xmlValue)
+
+final<-subset(zippy,tt$zippy==21224)
+
+#library(data.table)
+
+#DT = data.table(x=c("b","b","b","a","a"),v=rnorm(5))
+
+#DT<-fread('getdata-data-ss06pid.csv')
+
+#head(DT)
+
+#print (system.time (mean(DT$pwgtp15,by=DT$SEX)))
+#system.time (rowMeans(DT)[DT$SEX==1]; rowMeans(DT)[DT$SEX==2])
+#print (system.time (sapply(split(DT$pwgtp15,DT$SEX),mean)))
+#print (system.time (DT[,mean(pwgtp15),by=SEX]))
+#print (system.time (tapply(DT$pwgtp15,DT$SEX,mean)))
+#system.time (mean(DT[DT$SEX==1,]$pwgtp15); mean(DT[DT$SEX==2,]$pwgtp15))
---
+++ run_analysis.R
@@ -0,0 +1,5 @@
+#1Merges the training and the test sets to create one data set.
+#2Extracts only the measurements on the mean and standard deviation for each measurement. 
+#3Uses descriptive activity names to name the activities in the data set
+#4Appropriately labels the data set with descriptive variable names. 
+#5From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
