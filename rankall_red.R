7807b249e2bc388a966c42a6bb7cee1caf0b9bab
jerry dumblauskas <jdumblauskas@gmail.com>
2015-03-17 07:05:32 -0500

    sub 20

--- rankall_red.R
+++ rankall_red.R
@@ -1,15 +1,12 @@
 rankall_red <- function(outcome, num = "best") {
     dt <-read.csv("~/r_work/rprog-data-ProgAssignment3-data/outcome-of-care-measures.csv", colClasses = "character")
-    outcomes<-c("heart attack", "heart failure", "pneumonia")
-    idx<-c(11,17,23)
-    if (!match(outcome,outcomes, nomatch=0)) stop("invalid outcome")
-    qaz<-data.frame(idx,outcomes)
-    ind<-subset(qaz$idx,qaz$outcomes==outcome)
+    qaz<-data.frame(c(11,17,23),c("heart attack", "heart failure", "pneumonia"))
+    if (!match(outcome,qaz[,2], nomatch=0)) stop("invalid outcome")
     state.abbr <-  c (state.abb[1:8], "DC", state.abb[9:44], 'VI', state.abb[45:50])
     rtn = matrix(nrow=52, ncol=2, dimnames=list(rep(NA,52), c("hospital", "state")))
     x<-1
     for (l_st in state.abbr[order(state.abbr)] ) {
-        d_final<-subset(dt,dt[7]==l_st & dt[ind] != 'Not Available', select=c(2,ind))
+        d_final<-subset(dt,dt[7]==l_st & dt[subset(qaz[,1],qaz[,2]==outcome)] != 'Not Available', select=c(2,subset(qaz[,1],qaz[,2]==outcome)))
         d_final[[2]]<-as.numeric(d_final[[2]])
         numx<-num
         numx<-ifelse(num == "worst",nrow(d_final), numx)
