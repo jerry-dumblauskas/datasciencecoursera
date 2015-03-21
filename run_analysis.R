4809aa5fa3019354d9c15ade1b986b78aa9a2fcb
jerry dumblauskas <jdumblauskas@gmail.com>
2015-03-21 12:33:16 -0500

    final commit here -- for this project

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,11 @@
 #1Merges the training and the test sets to create one data set.
+l_tst_dt       <- read.table("./UCI HAR Dataset/test/X_test.txt", sep="")
+l_tst_person   <- read.table("./UCI HAR Dataset/test/subject_test.txt", sep="")
+l_tst_activity <- read.table("./UCI HAR Dataset/test/y_test.txt", sep="")
+
+l_trn_dt       <- read.table("./UCI HAR Dataset/train/X_train.txt", sep="")
+l_trn_person   <- read.table("./UCI HAR Dataset/train/subject_test.txt", sep="")
+l_trn_activity <- read.table("./UCI HAR Dataset/train/y_train.txt", sep="")
 #2Extracts only the measurements on the mean and standard deviation for each measurement. 
 #3Uses descriptive activity names to name the activities in the data set
 #4Appropriately labels the data set with descriptive variable names. 
